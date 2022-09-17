5e31ca88b427180dfa1737cd22426ccbad94da56
Simon-Fontaine <simon.fontaine@live.com>
2022-09-17 15:18:13 +0200

    testing github actions 2

--- dist/server.js
+++ dist/server.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // @ts-ignore
 var express_1 = __importDefault(require("express"));
 var app = (0, express_1.default)();
-var port = 80;
+var port = 3000;
 app.get("/", function (req, res) { return res.send("Hello World!"); });
-app.listen(port, function () { return console.log("App listening at http://217.69.8.104:".concat(port)); });
+app.listen(port, function () { return console.log("[WEB] Listening to http://217.69.8.104:".concat(port)); });
+console.log("[WEB] Website Up and Runing!");
